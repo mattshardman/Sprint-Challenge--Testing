cb776e68c45c7842f1771d6b636475b7d6a7aff5
mattshardman <mattshardman@gmail.com>
2019-04-12 08:47:47 +0100

    add more tests for post endpoint

--- routes/gameRoutes.test.js
+++ routes/gameRoutes.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const routes = require('../server/server');
+const games = require('../data');
 
 describe('test endpoint', () => {
     it('returns a status code of 200', () => {
@@ -12,7 +13,23 @@ describe('test endpoint', () => {
 describe('test endpoints', () => {
     describe('POST /games endpoint', () => {
         it('returns a status code 201', () => {
+            return request(routes)
+                .post('/games')
+                .expect(201);
+        });
 
+        it('games array has length of 3', () => {
+            return request(routes)
+                .post('/games')
+                .send({
+                    title: 'Pacman', // required
+                    genre: 'Arcade', // required
+                    releaseYear: 1980 // not required
+                })
+                .then((r) => {
+                    expect(games.length).toBe(1);
+                    expect(r.length).toBe(1);
+                })
         });
     });
 });
